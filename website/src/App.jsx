876ac86ae88971a788cafa7bd421280f9b972e3e
Yugaraj-medtek <yugaraj@medtekviyab.com>
2025-02-21 10:30:42 +0530

    removed aboutus and contact page

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
-import About from './components/Aboutus/About';
-import Contact from './components/Contactus/Contact';
 import Homepage from './components/HomePage/Homepage';
 import Footer from './components/Footer/Footer';
 
@@ -12,8 +10,6 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer/>
     </>
--- website/src/components/HomePage/Homepage.jsx
+++ website/src/components/HomePage/Homepage.jsx
@@ -1,7 +1,7 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useInView } from "react-intersection-observer";
 import "../HomePage/Homepage.css";
-import OurServicesSection from "../HomePage/OurServiceSection";
 
 const Homepage = () => {
   const cardRef = useRef(null);
@@ -35,6 +35,139 @@ const Homepage = () => {
     };
   }, []);
 
+  const parallaxRef = useRef(null);
+
+  // Intersection observer for section animation
+  const { ref: sectionRef, inView: sectionInView } = useInView({
+    threshold: 0.1,
+    triggerOnce: true,
+  });
+
+  // List of items for the "How It Works" section
+  const listItems = [
+    {
+      number: "01",
+      title: "Efficiency",
+      description:
+        "We help teams become more efficient by providing valuable insights and automation.",
+    },
+    {
+      number: "02",
+      title: "Reliability",
+      description:
+        "Our solutions are built with precision and consistency to ensure dependable performance.",
+    },
+    {
+      number: "03",
+      title: "Scalability",
+      description:
+        "We design systems that grow with your business, adapting to evolving needs effortlessly.",
+    },
+    {
+      number: "04",
+      title: "Security",
+      description:
+        "Data integrity and protection are at the core of our solutions, ensuring compliance and safety.",
+    },
+  ];
+
+  // GOOD AT WORK
+  const services = [
+    {
+      title: "Data Engineering",
+      description:
+        "Build and maintain systems for collecting, storing, processing, and analyzing your data efficiently and at scale.Our focus is always on Mission Success, and we are very pragmatic when choosing the best tools for the job.",
+      icon: "/data-eng.png",
+    },
+    {
+      title: "Data Analytics",
+      description:
+        "With analytics, we help our partners gain valuable insights into their operations and performance. Analytics allows teams to get transparency on ground realities, and lets organizations make data-driven decisions.",
+      icon: "/data-analytics.png",
+    },
+    {
+      title: "Data Science & Machine Learning",
+      description:
+        "Build and deploy production grade machine learning systems and pipelines at scale to support features like:",
+      icon: "/machine-learning.png",
+      list: [
+        "Personalized User Feeds.",
+        "Recommendation Systems.",
+        "Fraud Detection and Prevention.",
+        "Customer Churn Prediction and many many more.",
+      ],
+    },
+    {
+      title: "Data Security",
+      description:
+        "Design systems and processes for securing mission critical data like:",
+      icon: "/data-security.png",
+      list: [
+        "Automated Auditing and Monitoring data access.",
+        "Backup and Disaster Recovery.",
+        "Fine-grained Data Access Control systems.",
+        "Identify and Classify Data by sensitivity, value and impact.",
+      ],
+    },
+  ];
+
+  // Parallax effect on scroll
+  useEffect(() => {
+    const handleScroll = () => {
+      if (parallaxRef.current) {
+        const scrollPosition = window.pageYOffset;
+        parallaxRef.current.style.transform = `translateY(${
+          scrollPosition * 0.5
+        }px)`;
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+   const [formData, setFormData] = useState({
+      name: "",
+      email: "",
+      companyname: "",
+      message: "",
+    });
+  
+    const handleChange = (e) => {
+      setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+  
+    const handleSubmit = async(e) => {
+      e.preventDefault();
+      console.log('Form submitted:', formData);
+      const data = new FormData();
+      data.append('name', formData.name);
+      data.append('email', formData.email);
+      data.append('companyname', formData.companyname);
+      data.append('message', formData.message);
+  
+      const Sheet_Url="https://script.google.com/macros/s/AKfycbykhtbbgzewRbqiEJbgEYNKFZvFgKY6DT6M3miXsKwWerJ0L_fLOKharM5lqMMeVJ8/exec"
+      try {
+        await fetch(Sheet_Url, {
+          method: 'POST',
+          body: data,
+          muteHttpExceptions: true,
+        });
+  
+        setFormData({
+          name: '',
+          email: '',
+          companyname: '',
+          message: '',
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  
   return (
     <>
       <div className="homepage flex flex-col items-start justify-start p-5 sm:p-10">
@@ -186,10 +319,6 @@ const Homepage = () => {
         </div>
       </div>
 
-      {/* Our Service Card Section */}
-      <OurServicesSection />
-
-
       {/* Innovative Content Section */}
       <section className="about-section-homepage">
         <div className="container-innovate">
@@ -220,104 +349,168 @@ const Homepage = () => {
           </div>
         </div>
       </section>
-      
-      {/* Reviews Section */}
-      {/* <section className="testimonial-section mx-auto">
-        <div className="review-container">
-          <div className="heading1">
-            <p className="subheading1">REVIEWS</p>
-            <h2>Our Customers Reviews</h2>
+
+      {/* HOW IT WORKS */}
+      <section ref={sectionRef} className="about-section">
+        <div className="about-container">
+          <div className="content-wrapper">
+            <div className="left-content-about">
+              <div className="about-header">
+                <p className="subheading">HOW IT WORKS</p>
+                <h2 className="main-title">
+                  Our Journey Towards Technological Excellence
+                </h2>
+              </div>
+            </div>
+            <div className="right-content-about">
+              <h1 className="heading-about">
+                At Datalytix, we specialize in providing innovative software
+                solutions that enable businesses to streamline operations,
+                enhance productivity, and achieve their goals efficiently.
+              </h1>
+              <ul className="list-none p-0">
+                {listItems.map((item, index) => (
+                  <li
+                    key={index}
+                    className={`mb-6 opacity-0 transform translate-x-0 transition-all duration-500 ease-out ${
+                      sectionInView ? "opacity-100 translate-x-0" : ""
+                    }`}
+                    style={{ transitionDelay: `${index * 500}ms` }}
+                  >
+                    <div className="flex items-center mb-2">
+                      <span className="number mr-2">{item.number}</span>
+                      <h3 className="title">{item.title}</h3>
+                    </div>
+                    <p className="description">{item.description}</p>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
-          <div className="cards">
-            <div className="card-container">
-              <div className="testimonial-card">
-                <div className="stars">
-                  <span className="star"></span>
-                  <span className="star"></span>
-                  <span className="star"></span>
-                  <span className="star"></span>
-                  <span className="star"></span>
-                </div>
-                <p className="testimonial-text">
-                  Datalytix has been an amazing investment partner for me. Their
-                  investment management services are easy to use.
-                </p>
-                <p className="testimonial-author">@ Yury</p>
-              </div>
-              <div className="testimonial-card">
-                <div className="stars">
-                  <span className="star"></span>
-                  <span className="star"></span>
-                  <span className="star"></span>
-                  <span className="star"></span>
-                  <span className="star"></span>
-                </div>
-                <p className="testimonial-text">
-                  The services provided by Datalytix helped me achieve my
-                  investment goals easily and effectively.
-                </p>
-                <p className="testimonial-author">@ Alex</p>
-              </div>
-              <div className="testimonial-card">
-                <div className="stars">
-                  <span className="star"></span>
-                  <span className="star"></span>
-                  <span className="star"></span>
-                  <span className="star"></span>
-                  <span className="star"></span>
-                </div>
-                <p className="testimonial-text">
-                  I am so glad I chose Datalytix for my financial needs. Their
-                  team is very supportive and knowledgeable.
-                </p>
-                <p className="testimonial-author">@ John</p>
-              </div>
-              <div className="testimonial-card">
-                <div className="stars">
-                  <span className="star"></span>
-                  <span className="star"></span>
-                  <span className="star"></span>
-                  <span className="star"></span>
-                  <span className="star"></span>
-                </div>
-                <p className="testimonial-text">
-                  Thanks to Datalytix, my investments are growing steadily. I
-                  highly recommend their services.
-                </p>
-                <p className="testimonial-author">@ Sarah</p>
-              </div>
-              <div className="testimonial-card">
-                <div className="stars">
-                  <span className="star"></span>
-                  <span className="star"></span>
-                  <span className="star"></span>
-                  <span className="star"></span>
-                  <span className="star"></span>
-                </div>
-                <p className="testimonial-text">
-                  Datalytix has been an amazing investment partner for me. Their
-                  investment management services are easy to use.
-                </p>
-                <p className="testimonial-author">@ Joel</p>
-              </div>
-              <div className="testimonial-card">
-                <div className="stars">
-                  <span className="star"></span>
-                  <span className="star"></span>
-                  <span className="star"></span>
-                  <span className="star"></span>
-                  <span className="star"></span>
-                </div>
-                <p className="testimonial-text">
-                  Datalytix has been an amazing investment partner for me. Their
-                  investment management services are easy to use.
-                </p>
-                <p className="testimonial-author">@ Noel</p>
+        </div>
+      </section>
+
+      {/* What We're Good at */}
+      <div className="bg-black text-white p-8">
+        <div className="max-w-7xl mx-auto">
+          <div className="flex flex-wrap -mx-6">
+            <div className="w-full lg:w-1/3 px-4 mb-8 lg:mb-0">
+              <h2 className="text-4xl font-bold mb-4">
+                What We're <span className="text-purple-400">Good at.</span>
+              </h2>
+            </div>
+            <div className="w-full lg:w-2/3 px-4">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                {services.map((service, index) => (
+                  <div key={index} className="pb-8">
+                    <img
+                      src={service.icon}
+                      alt={`${service.title} icon`}
+                      className="w-20 h-20 mb-6"
+                    />
+                    <h3 className="text-2xl font-semibold mb-4">
+                      {service.title}
+                    </h3>
+                    <p className="text-lg mb-4">{service.description}</p>
+                    {service.list && (
+                      <ul className="list-disc list-inside">
+                        {service.list.map((item, i) => (
+                          <li key={i} className="text-xm mb-2">
+                            {item}
+                          </li>
+                        ))}
+                      </ul>
+                    )}
+                  </div>
+                ))}
               </div>
             </div>
           </div>
         </div>
-      </section> */}
+      </div>
+
+      {/* GET IN TOUCH */}
+      <section className="contact-section">
+        <div className="content">
+          <div className="left-content-contact">
+            <p className="contact-title">GET IN TOUCH</p>
+            <h2 className="contact-subtitle">
+              Reach out to us for any help and inquiries
+            </h2>
+          </div>
+          <div
+            className={`right-content-contact ${
+              sectionInView ? "in-view" : ""
+            }`}
+            ref={sectionRef}
+          >
+            <div className="input-form">
+              <form method="POST" className="form" onSubmit={handleSubmit}>
+                <div className="form-group">
+                  <label htmlFor="your-name" className="form-label">
+                    Your Name
+                  </label>
+                  <input
+                    id="name"
+                    name="name"
+                    type="text"
+                    className="form-input"
+                    value={formData.name}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email-address" className="form-label">
+                    Email Address
+                  </label>
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    className="form-input"
+                    value={formData.email}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="company-name" className="form-label">
+                    Company Name
+                  </label>
+                  <input
+                    id="companyname"
+                    name="companyname"
+                    type="text"
+                    className="form-input"
+                    value={formData.companyname}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="message" className="form-label">
+                    Message
+                  </label>
+                  <textarea
+                    id="message"
+                    name="message"
+                    className="form-input"
+                    value={formData.message}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <button type="submit" className="submit-button">
+                    Send Message
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
